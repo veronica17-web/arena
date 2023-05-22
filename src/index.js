452524f513aee7307b395db58da9c47e65ff8b0f
ProcureN <132357850+ProcureN@users.noreply.github.com>
2023-05-22 16:12:52 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose")
 const route = require("./router/router")
 
 const app = express()
-// const multer= require("multer");
+const multer= require("multer");
 
-// app.use( multer().any())
+app.use( multer().any())
 app.use(express.json());
-// var cors = require('cors');
-// app.use(cors());
+var cors = require('cors');
+app.use(cors());
 
 mongoose.connect("mongodb+srv://broaddcast-arena:niGWGzHNuZHyyMnN@cluster0.w7swjdn.mongodb.net/arena", {
     useNewUrlParser: true
